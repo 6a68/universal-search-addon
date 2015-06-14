e1cc1c73bb5ac8bec84bfcbacf1e502423b49178
Jared Hirsch <ohai@6a68.net>
2015-06-14 10:42:39 -0700

    finish converting to restartless
      - addon now works
      - attaching correctly to all existing windows
      - not sure the XBL is actually attaching to the popup, still :-(

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,12 @@
-var {Cc, Ci, Cu} = require("chrome");
-Cu.import('resource://gre/modules/XPCOMUtils.jsm');
+// TODO: bootstrapped extensions cache strings, scripts, etc forever.
+//       figure out when and how to cache-bust.
+//       bugs 918033, 1051238, 719376
 
-var winMediator = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator);
-var win = winMediator.getMostRecentWindow('navigator:browser');
-var doc = win.document;
-win.LOL = {};
+// var {Cc, Ci, Cu} = require("chrome");
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+var Cu = Components.utils;
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 
 function UniversalSearch() {
   this.iframeURL = 'https://mozilla.org';
@@ -12,30 +14,27 @@ function UniversalSearch() {
 UniversalSearch.prototype = {
   constructor: UniversalSearch,
   // TODO: hide the second search bar with ToolbarButtonManager
-  render: function() {
+  render: function(win) {
     console.log("entering render");
-
+    var doc = win.document;
     this.urlbar = doc.getElementById('urlbar');
     this.popup = doc.getElementById('PopupAutoCompleteRichResult');
 
-    // use the Stylesheet Service to load in the CSS containing the -moz-binding XBL magic
-    var sss = Cc["@mozilla.org/content/style-sheet-service;1"]
-              .getService(Ci.nsIStyleSheetService);
-    var ios = Cc["@mozilla.org/network/io-service;1"]
-              .getService(Ci.nsIIOService);
+    var sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+    var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
     var escapedCSS = encodeURIComponent('#PopupAutoCompleteRichResult.PopupAutoCompleteRichResultUnivSearch { ' +
-    '-moz-binding: url("' + self.path.chrome + '/content/content.xml#autocomplete-rich-result-popup-univ-search"); }');
+      '-moz-binding: url("chrome://universalsearch/content/content.xml#autocomplete-rich-result-popup-univ-search"); }');
     var uri = ios.newURI('data:text/css,' + escapedCSS, null, null);
     // TODO: performance thing: loadAndRegisterSheet is synchronous.
     //       However, the IDL doesn't mention any async alternative.
+    console.log('typeof sss: ' + typeof sss);
     sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
 
+    console.log('typeof this.popup: ' + typeof this.popup);
     this.popup.setAttribute("class", 'PopupAutoCompleteRichResultUnivSearch');
     this.popup._appendCurrentResult = function() {
       console.log('popup._appendCurrentResult');
     };
-
-    console.log('exiting render');
   },
   onLoaded: function() {
     console.log('entering onLoaded');
@@ -56,9 +55,66 @@ UniversalSearch.prototype = {
   },
   _appendCurrentResult: function() {
   }
-}
-
-var startup = function(aData, aReason) {
-  win.LOL.univSearch = new UniversalSearch();
-  win.LOL.univSearch.render();
 };
+
+// nsIWindowMediatorListener: onOpenWindow, onCloseWindow, onWindowTitleChange
+// TODO: should this just be part of the UniversalSearch object? "when your window is ready, render yourself into it"
+var mediatorListener = {
+  onOpenWindow: function(win) {
+    console.log('onOpenWindow');
+    var domWindow = win.QueryInterface(Ci.nsIInterfaceRequestor)
+                       .getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", this.onDOMWindowLoaded.bind(this, domWindow));
+  },
+  onDOMWindowLoaded: function(domWindow) {
+    domWindow.removeEventListener("load", this.onDOMWindowLoaded.bind(this, domWindow), false);
+    this.render(domWindow);
+  },
+  onCloseWindow: function(win) {
+    console.log('onCloseWindow');
+    this.derender(win);
+  },
+  onWindowTitleChange: function(win, title) {
+    // no-op 
+  },
+  render: function(win) {
+    // TODO: it looks like the XUL window is not writable. so I guess instead of
+    //       having a global on the window, I'll just have globals in here?
+    var univSearch = new UniversalSearch();
+    univSearch.render(win);
+  },
+  derender: function(win) {
+    // probably should actually fire a shutdown signal,
+    // pass a shutdown promise,
+    // every piece of the app shuts itself down then resolves its bit,
+    // when the base promise resolves, actually delete win.LOL.
+  }
+};
+
+function startup(aData, aReason) {
+  // "inject code into all windows and listen for new windows" code courtesy of Mossop
+  // http://www.oxymoronical.com/blog/2011/01/Playing-with-windows-in-restartless-bootstrapped-extensions
+  var winMediator = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator);
+
+  winMediator.addListener(mediatorListener);
+
+  var windows = winMediator.getEnumerator('navigator:browser');
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    mediatorListener.render(domWindow);
+  }
+  console.log('exiting universal-search-addon startup');
+};
+
+var shutdown = function(aData, aReason) {
+  // no teardown is needed for a normal shutdown
+  if (reason == APP_SHUTDOWN) { return; }
+
+  var winMediator = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator);
+  winMediator.removeListener(mediatorListener);
+};
+
+function install() { console.log('installing universal-search-addon') }
+function uninstall() { console.log('unnstalling universal-search-addon') }
+function shutdown() { console.log('shutting down universal-search-addon') }
+
